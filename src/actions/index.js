28941ba5aa25b7079e855c9f7d4d2c035c99f27d
Sagar Patil <saagar.1647@gmail.com>
2017-12-04 23:28:05 +0530

    Add: Axios get method call

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 const API_KEY = 'b3d6d5fb50932dfe0725c222ac3bdf7e';
 ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
@@ -6,8 +8,10 @@ export const FETCH_WEATHER = 'FETCH_WEATHER';
 // Action creators always have to return an action and an action is an object which always return a type.
 export function fetchWeather(city) {
   const url = `{ROOT_URL}&q={city},us`;
+  const request = axios.get(url);
 
   return {
-    type: FETCH_WEATHER
+    type: FETCH_WEATHER,
+    payload: request
   };
 }
