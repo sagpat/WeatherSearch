dccf5d54a269f37274c3d7963edd8efcc49d9815
Sagar Patil <saagar.1647@gmail.com>
2017-12-04 23:22:10 +0530

    Add: Actions and Axios Library

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,13 @@
 const API_KEY = 'b3d6d5fb50932dfe0725c222ac3bdf7e';
+ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+// Action creators always have to return an action and an action is an object which always return a type.
+export function fetchWeather(city) {
+  const url = `{ROOT_URL}&q={city},us`;
+
+  return {
+    type: FETCH_WEATHER
+  };
+}
--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import ReduxPromise from 'redux-promise';
 
 import App from './components/app';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+// middleware has the ability to block, modify and just pass through the actions that are created before they head to reducers.  
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
